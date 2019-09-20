da292bcf9e6513b82d6282f870fd273dc759f22a
Geoage <51729379+user22087@users.noreply.github.com>
2019-09-20 21:01:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,14 +29,6 @@ def callback():
          sendText(user,'ว่าไง')
     elif (userText == 'ทำไรอยู่') :
          sendText(user,'เล่นเกม')
-    elif (userText == 'ฟ้องพี่เจคแน่') :
-         sendText(user,'หยุดเลย!')
-    elif (userText == 'อายุเท่าไหร่แล้ว') :
-         sendText(user,'23')
-    elif (userText == 'ชื่อไรนะ') :
-         sendText(user,'แฮรริสัน')
-    elif (userText == 'พูดอีกทีได้ป่ะ') :
-         sendText(user,'-*-')
     elif (userText == 'ชื่ออะไร') :
          sendText(user,'แฮริสันออสเตอร์ฟิลด์')
     else :
